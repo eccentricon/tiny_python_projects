bf20810a6863c9486e15bbe471bb4ed86d35e1f1
Ted Osborne <ted@tedosborne.com>
2020-10-22 07:57:33 -0400

    Completed exercise 07

---
+++ 07_gashlycrumb/gashlycrumb.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Author : Ted Osborne <ted@tedosborne.com>
+Date   : 2020-10-20
+Purpose: Gashlycrumb
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Gashlycrumb',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('letter',
+                        metavar='letter',
+                        nargs='+',
+                        type=str,
+                        help='letter(s)')
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='input file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default='gashlycrumb.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    gashlycrumbs = { line[0].upper(): line.rstrip() for line in args.file }
+
+    for letter in args.letter:
+        print(gashlycrumbs.get(letter.upper(), f'I do not know "{letter}".'))
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
